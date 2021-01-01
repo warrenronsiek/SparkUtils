b8debab9ec8abeda938fe28218a3c2656e060c14
warrenronsiek <warren@warren-r.com>
2021-01-01 13:15:48 -0800

    needs even moar tests

--- src/test/scala/com/warren_r/sparkutils/SnapshotTestTest.scala
+++ src/test/scala/com/warren_r/sparkutils/SnapshotTestTest.scala
@@ -14,13 +14,19 @@ class SnapshotTestTest extends AnyFlatSpec with SnapshotTest {
     StructField("val2", IntegerType, nullable = false)
   ))
   val goodDf: DataFrame = sparkSession.read.schema(schema).csv(getClass.getResource("/testdata.csv").getPath)
-  val badDf: DataFrame =  sparkSession.read.schema(schema).csv(getClass.getResource("/badtestdata.csv").getPath)
+  val badDf: DataFrame = sparkSession.read.schema(schema).csv(getClass.getResource("/badtestdata.csv").getPath)
+
+  "snapshot path creation" should "create correct paths" in {
+    assert(snapshotPath("testpath") ==
+      List(System.getProperty("user.dir"), "src", "test", "resources", "com", "warren_r", "sparkutils",
+        "snapshottesttest", "testpath").mkString("/"))
+  }
 
   "snapshot testing" should "validate snapshots" in {
     assertSnapshot("gooddata", goodDf, List("id"))
   }
 
-//  it should "invalidate invalid snapshots" in {
-//
-//  }
+  it should "invalidate invalid snapshots" in {
+    assert(!compareSnapshot(badDf, goodDf, List("id")))
+  }
 }
